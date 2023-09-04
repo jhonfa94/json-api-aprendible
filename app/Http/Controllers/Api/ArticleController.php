5d10862d7d16811d45aae48dd69aa65902e78c84
Jhon Fabio Cardona Martinez <fabio.cardona@coytex.com.co>
2023-09-03 22:34:57 -0500

    Implementando la ruta para crear articulos

---
+++ app/Http/Controllers/Api/ArticleController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ArticleCollection;
+use App\Http\Resources\ArticleResource;
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    //
+    public function index(): ArticleCollection
+    {
+        return ArticleCollection::make(Article::all());
+    }
+
+    public function show(Article $article)
+    {
+        return ArticleResource::make($article);
+    }
+
+    public function create(Request $request)
+    {
+        $article = Article::create([
+            'title' => $request->input('data.attributes.title'),
+            'slug' => $request->input('data.attributes.slug'),
+            'content' => $request->input('data.attributes.content'),
+
+        ]);
+        return ArticleResource::make($article);
+    }
+}
